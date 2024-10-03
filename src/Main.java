e9098b53b01b2d1b84ed1056cb04ed5282d97a78
Nacho Diez <diezgarcianacho@gmail.com>
2024-10-04 01:11:56 +0200

    Iguales, mayor y menor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+
+        if (number1<number2) {
+            System.out.println(number2 + "es el mayor");
+        }else{
+            if(number2>number1){System.out.println(number1 + "es el mayor");
+            }else{
+                System.out.println("Los numeros son iguales");
+            }
+        }
     }
 }
